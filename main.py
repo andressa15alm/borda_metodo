1513ca4744f18ad9c9d3ffb72d15b847c88cded6
Paula Andressa <88633758+andressa15alm@users.noreply.github.com>
2023-08-16 00:46:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,21 @@ import pandas as pd
 num_alternativas = int(input("Quantas alternativas você deseja inserir? "))
 respostas = []
 
-criterios = []
 
 num_criterios = int(input("Quantos criterios você deseja inserir? "))
+criterios = []
 
 for i in range(num_alternativas):
     resposta = input("Digite a alternativa {}: ".format(i+1))
     respostas.append(resposta)
 
 
-
-
 for i in range(num_criterios):
     criterio = input("Digite o criterio {}: ".format(i+1))
     criterios.append(criterio)
 
-
-
-classificacoes = {}
 # Coletar a classificação das alternativas para cada critério
+classificacoes = {}
 for criterio in criterios:
     classificacoes[criterio] = {}
     print("Classificação para o critério", criterio)
@@ -32,10 +28,10 @@ for criterio in criterios:
 
 
 
-# Criar o DataFrame com as classificações
+# DataFrame com as classificações
 df = pd.DataFrame(classificacoes, index=respostas)
 df["Total"] = df.sum(axis=1)
-# Adicionar uma coluna "Ordenação Final" com as alternativas ordenadas em ordem crescente com base na coluna "Total"
+
 df["Ordenação Final"] = df["Total"].rank()
 print("\nTabela de Classificações:\n")
 print(df)
